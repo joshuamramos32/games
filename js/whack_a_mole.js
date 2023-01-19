e9aa80a9f43056a27c9ef6ca319236ed623f0dbb
Joshua Ramos <joshua.m.ramos32@gmail.com>
2023-01-19 16:52:57 -0600

    images need to disappear on game strt

--- js/whack_a_mole.js
+++ js/whack_a_mole.js
@@ -1,80 +1,79 @@
 "use strict";
+$(document).ready(function(){
+
+	const holes = document.querySelectorAll(".square.hole.mole");
+	const scoreBoard = document.querySelector(".score");
+	const moles = document.querySelectorAll(".mole");
+	// const startButton = document.querySelector(".StartButton")
+	let lastHole;
+	let timeUp = false;
+	let score = 0;
 
 
-const holes =$(".square.hole");
-const scoreBoard = $(".score");
-const moles = $(".mole");
-let lastHole;
-let timeUp = false;
-let score = 0;
 
 
 
+	function randomTime(min,max){
+		return Math.round(Math.random() * (max - min) + min);
 
-
-function randomTime(min,max){
-	return Math.floor(Math.random() * (max - min-1) + min);
-}
-
-function randomHole(holes){
-	const idx = Math.floor(Math.random() + holes.length );
-	const hole = holes[idx];
-	if (hole === lastHole){
-		return randomHole(holes);
 	}
-	lastHole = hole;
-	return hole;
-}
-function peep(){
-	const time = randomTime(200, 1000);
-	const hole = randomHole(holes);
-	hole.classList.add("up");
-	setTimeout(() => {
-		hole.classList.remove("up");
-		if(timeUp) peep();
-	}, time);
-}
-function startGame(){
-	$(".button").click(scoreBoard.textContent = 0);
-	timeUp = true;
-	score = 0;
-	peep();
-	setTimeout(() => timeUp = true, 1000);
-	setInterval(peep,1500);
-
-}
-startGame()
 
 
-function whack(e){
-	if(!e.isTrusted) return;
-	score++;
-	this.parentNode.classList.remove("up");
-	scoreBoard.textContent = score;
+
+	function randomHole(holes){
+		const idx = Math.floor(Math.random() * holes.length );
+		const hole = holes[idx];
+		if (!hole === lastHole){
+			return randomHole(holes);
+		}
+		lastHole = hole;
+		return hole;
+	}
+
+	function peep(){
+		const time = randomTime(200, 500);
+		const mole = randomHole(moles);
+		$('.img').on("click", function (){
+			$(this).toggleClass("hidden");
+		})
+		// mole.classList.add('down');
+		setTimeout(() => {
+			mole.classList.toggle('up');
+			if(timeUp) peep();
+		}, time);
+
+	}
+
+	function startGame() {
+		$(".StartButton").click(() => {
+			scoreBoard.textContent = 0;
+			timeUp = true;
+			score = 0;
+			peep();
+			setTimeout(() => timeUp = false, 9000);
+		});
+
+	}
+startGame();
+
+
+
+	function whack(){
+		if(!timeUp) return;
+		score++;
+		this.parentNode.classList.remove("up");
+		scoreBoard.textContent = score;
+		console.log(this);
+		console.log(this.classList);
+	}
+
 	moles.forEach(mole => mole.addEventListener('click', whack));
-}
-// moles.forEach(mole => mole.addEventListener('click', whack));
+	console.log(moles);
 
 
-// window.addEventListener("load",startGame)
-// function startGame(){
-// 	$( ".button" ).click(function() {
-// 		$( "img" ).fadeOut( "slow" );
-// 	});
-// };
 
-// var squares = document.getElementsByClassName("square")
-// function randomMoles(){
-// 	let randomsquare= Math.floor(Math.random() * ((10)* squares.length));
-// 	for (let i = 0; i < squares.length; i++){
-// 	if (randomsquare === 0) {
-// 		 $("img").fadeIn(1000);
-//
-// 	}
-// }
-// }
-//
-// randomMoles(squares);
+});
+
 
 
 
